25c91349b8456b7a14dcc1a20984f3be926ad27c
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-19 19:30:55 -0400

    "Customer View Models"

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
+using GeneralStoreMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<GeneralStoreDbContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("GeneralStoreDb")
+    )
+);
 
 var app = builder.Build();
 
