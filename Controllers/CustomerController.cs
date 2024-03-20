eeab230c3448d8f61d2248fd3c05ae0d8a67111b
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-19 20:36:30 -0400

    "Customer create controller action / view"

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -29,5 +29,35 @@ namespace GeneralStoreMVC.Controllers
 
             return View(customers);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CustomerCreateViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ErrorMsg"] = "Model State is invalid.";
+                return View(model);
+            }
+
+            Customer entity = new()
+            {
+                Name = model.Name,
+                Email = model.Email
+            };
+                _ctx.Customers.Add(entity);
+
+            if (await _ctx.SaveChangesAsync() != 1)
+            {
+                TempData["ErrorMsg"] = "Unable to save to the database. Please try again later.";
+                return View(model);
+            }
+
+                return RedirectToAction(nameof(Index));
+        }
     }
 }
