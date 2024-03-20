91fd5e3efc11c4e8a185b6367a68d2accb7af6d1
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-19 21:48:04 -0400

    "Add Transaction Model"

---
+++ Models/Transaction/TransactionCreateVM.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeneralStoreMVC.Models.Transaction
+{
+    public class TransactionCreateVM
+    {
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        [Display(Name = "Product")]
+        public int ProductId { get; set; }
+
+        [Required]
+        [Range(1, 100)]
+        public int Quantity { get; set; }
+    }
+}
